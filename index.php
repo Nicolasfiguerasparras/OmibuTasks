04c6d112a879f6bbe68f1b03d4c9d579d31d7147
Nicolasfiguerasparras <36232758+Nicolasfiguerasparras@users.noreply.github.com>
2019-05-16 20:09:01 +0200

    sm

--- index.php
+++ index.php
@@ -151,7 +151,7 @@
 												<!-- Generic view -->
 													<div class="tab-pane fade show active" id="nav-first" role="tabpanel" aria-labelledby="genericView">
 
-														<table class="table col-11">
+														<table class="table col-11 table-responsive-md">
 
 															<thead>
 																<tr class="table-primary">
@@ -227,7 +227,71 @@
 												
 												<!-- Calendar -->
 													<div class="tab-pane fade" id="nav-home" role="tabpanel" aria-labelledby="highPriority">
-														
+														<div class="col-12">
+															<table class="col-12 calendar">
+																<tr>
+																	<th>L</th>
+																	<th>M</th>
+																	<th>X</th>
+																	<th>J</th>
+																	<th>V</th>
+																	<th>S</th>
+																	<th>D</th>
+																</tr>
+																
+																<tr>
+																	<td>1</td>
+																	<td>2</td>
+																	<td>3</td>
+																	<td>4</td>
+																	<td>5</td>
+																	<td>6</td>
+																	<td>7</td>
+																</tr>
+
+																<tr>
+																	<td>8</td>
+																	<td>9</td>
+																	<td>10</td>
+																	<td>11</td>
+																	<td>12</td>
+																	<td>13</td>
+																	<td>14</td>
+																</tr>
+																
+																<tr>
+																	<td>15</td>
+																	<td>16</td>
+																	<td>17</td>
+																	<td>18</td>
+																	<td>19</td>
+																	<td>20</td>
+																	<td>21</td>
+																</tr>
+
+																<tr>
+																	<td>22</td>
+																	<td>23</td>
+																	<td>24</td>
+																	<td>25</td>
+																	<td>26</td>
+																	<td>27</td>
+																	<td>28</td>
+																</tr>
+
+																<tr>
+																	<td>29</td>
+																	<td>30</td>
+																	<td>31</td>
+																	<td></td>
+																	<td></td>
+																	<td></td>
+																	<td></td>
+																</tr>
+																
+															</table>
+
+														</div>
 													</div>
 												<!-- /Calendar -->
 
