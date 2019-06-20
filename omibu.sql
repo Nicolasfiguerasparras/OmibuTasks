5fa57236f32ac6a3714a65412ae9bb21e081cd4a
Nicolasfiguerasparras <36232758+Nicolasfiguerasparras@users.noreply.github.com>
2019-06-20 15:41:15 +0200

    Update omibu.sql

--- omibu.sql
+++ omibu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-05-2019 a las 09:18:13
+-- Tiempo de generación: 20-06-2019 a las 15:40:44
 -- Versión del servidor: 10.1.36-MariaDB
 -- Versión de PHP: 7.2.11
 
@@ -21,6 +21,25 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `omibu`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `client`
+--
+
+CREATE TABLE `client` (
+  `client_ID` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `client`
+--
+
+INSERT INTO `client` (`client_ID`, `name`) VALUES
+(1, 'Ómibu'),
+(2, 'iBaños');
 
 -- --------------------------------------------------------
 
@@ -63,7 +82,7 @@ CREATE TABLE `tareas` (
 --
 
 INSERT INTO `tareas` (`ID_tarea`, `Nombre`, `Descripcion`, `Fecha`, `Prioridad`, `Trabajador`, `Cliente`, `Finalizado`) VALUES
-(1, 'Ejemplo de tarea', 'Ejemplo de descripción de una tarea de prioridad alta', '2019-05-15', 1, 2, 1, 1),
+(1, 'Ejemplo de tarea', 'Ejemplo de descripción de una tarea de prioridad alta', '2019-05-15', 2, 2, 1, 1),
 (2, 'Ejemplo de tarea', 'Ejemplo de descripción de una tarea de prioridad media', '2019-05-22', 2, 2, 2, 0),
 (3, 'Ejemplo de tarea', 'Ejemplo de descripción de una tarea de prioridad baja', '2019-05-13', 3, 2, 1, 0),
 (10, 'Titulo de tarea', 'Esto es la descripción', '2020-01-01', 1, 2, 1, 0),
@@ -71,7 +90,34 @@ INSERT INTO `tareas` (`ID_tarea`, `Nombre`, `Descripcion`, `Fecha`, `Prioridad`,
 (12, 'Tarea ejemplo', 'oakkdsad', '2023-03-02', 3, 2, 2, 0),
 (13, 'asdf', 'asdfasdf', '2019-05-23', 2, 2, 2, 0),
 (14, 'Ejemplo de tare', 'Ejemplo de descripción de una tarea de prioridad alta', '2019-05-15', 1, 2, 0, 0),
-(15, 'Ejemplo de tarea', 'Ejemplo de descripción eee\r\n una tarea de prioridad alta', '2019-05-15', 1, 2, 0, 0);
+(15, 'Ejemplo de tarea', 'Ejemplo de descripción\r\n una tarea de prioridad alta', '2019-05-15', 1, 2, 0, 0),
+(16, 'Ejemplo de tareaaaa', 'Ejemplo de descripción de una tarea de prioridad alta', '2019-05-15', 1, 2, 0, 0),
+(17, 'Ejemplo de tarea', 'Ejemplo de descripción de una tarea de prioridad media', '2019-05-22', 1, 2, 0, 0),
+(18, 'Titulo de tarea', 'Esto es la descripción', '2020-01-01', 2, 2, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `task`
+--
+
+CREATE TABLE `task` (
+  `task_ID` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `description` varchar(500) COLLATE utf8_spanish2_ci NOT NULL,
+  `limit_date` date NOT NULL,
+  `priority` int(1) NOT NULL,
+  `worker_ID` int(5) NOT NULL,
+  `client_ID` int(5) NOT NULL,
+  `done` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `task`
+--
+
+INSERT INTO `task` (`task_ID`, `name`, `description`, `limit_date`, `priority`, `worker_ID`, `client_ID`, `done`) VALUES
+(1, 'Tarea ejemplo', 'Esto es un ejemplo de tarea ', '2019-05-22', 0, 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -98,9 +144,40 @@ INSERT INTO `trabajadores` (`ID_trabajador`, `Nombre`, `Apellidos`, `Direccion`,
 (1, 'Administrador', 'Omibu', 'Calle Cedrán, 4', 'Administrador', 'admin@omibu.com', 'admin', ''),
 (2, 'Nicolás', 'Figueras Parras', 'C\\ Javier Tortosa 10', 'Empleado en prácticas', 'nicolas@omibu.com', 'mAK0Lusaqa', '639941992');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `worker`
+--
+
+CREATE TABLE `worker` (
+  `worker_ID` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `surname` varchar(70) COLLATE utf8_spanish2_ci NOT NULL,
+  `address` varchar(200) COLLATE utf8_spanish2_ci NOT NULL,
+  `position` varchar(80) COLLATE utf8_spanish2_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
+  `password` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `phone` int(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `worker`
+--
+
+INSERT INTO `worker` (`worker_ID`, `name`, `surname`, `address`, `position`, `email`, `password`, `phone`) VALUES
+(1, 'Administrador', 'Ómibu', 'Calle Cedrán, 4', 'Administrador', 'admin@omibu.com', 'admin', 0),
+(2, 'Nicolás', 'Figueras Parras', 'Calle Javier Tortosa 10', 'Empleado en prácticas', 'nicolas@omibu.com', 'model1.0', 639941992);
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `client`
+--
+ALTER TABLE `client`
+  ADD UNIQUE KEY `client_ID` (`client_ID`);
 
 --
 -- Indices de la tabla `clientes`
@@ -117,6 +194,12 @@ ALTER TABLE `tareas`
   ADD UNIQUE KEY `ID_tarea` (`ID_tarea`);
 
 --
+-- Indices de la tabla `task`
+--
+ALTER TABLE `task`
+  ADD UNIQUE KEY `task_ID` (`task_ID`);
+
+--
 -- Indices de la tabla `trabajadores`
 --
 ALTER TABLE `trabajadores`
@@ -124,8 +207,20 @@ ALTER TABLE `trabajadores`
   ADD UNIQUE KEY `ID_trabajador` (`ID_trabajador`);
 
 --
+-- Indices de la tabla `worker`
+--
+ALTER TABLE `worker`
+  ADD UNIQUE KEY `worker_ID` (`worker_ID`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `client`
+--
+ALTER TABLE `client`
+  MODIFY `client_ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `clientes`
@@ -137,13 +232,25 @@ ALTER TABLE `clientes`
 -- AUTO_INCREMENT de la tabla `tareas`
 --
 ALTER TABLE `tareas`
-  MODIFY `ID_tarea` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `ID_tarea` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT de la tabla `task`
+--
+ALTER TABLE `task`
+  MODIFY `task_ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `trabajadores`
 --
 ALTER TABLE `trabajadores`
   MODIFY `ID_trabajador` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `worker`
+--
+ALTER TABLE `worker`
+  MODIFY `worker_ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
